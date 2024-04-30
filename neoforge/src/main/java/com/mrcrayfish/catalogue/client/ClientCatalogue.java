da672489e79983577d3de4d268d8e4943e2ea7c3
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-04-30 17:31:21 +0930

    🐛 Fixed incorrect event method on NeoForge

--- neoforge/src/main/java/com/mrcrayfish/catalogue/client/ClientCatalogue.java
+++ neoforge/src/main/java/com/mrcrayfish/catalogue/client/ClientCatalogue.java
@@ -14,7 +14,7 @@ import net.neoforged.fml.loading.FMLPaths;
 public class ClientCatalogue
 {
     @SubscribeEvent
-    private void onClientSetup(FMLClientSetupEvent event)
+    private static void onClientSetup(FMLClientSetupEvent event)
     {
         Config.load(FMLPaths.CONFIGDIR.get());
     }
